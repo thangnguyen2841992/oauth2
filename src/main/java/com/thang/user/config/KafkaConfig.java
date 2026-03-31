4110f12a79b88480d99c5b975133fa25de3b253d
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2026-03-31 22:55:37 +0700

    ll

---
+++ src/main/java/com/thang/user/config/KafkaConfig.java
@@ -0,0 +1,17 @@
+package com.thang.user.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    NewTopic checkSendActiveEmailTopic() {
+        return new NewTopic("send-email-active-response",2,(short) 1);
+    }
+//    NewTopic checkSendBirthDayTopic() {
+//        return new NewTopic("send-email-birthday-response",2,(short) 1);
+//    }
+
+}
