cb1ee84d816a59b8916f5aafb01a715ee2f8decd
thang <ntthang4@cmcglobal.vn>
2026-06-22 14:04:52 +0700

    test

--- src/main/java/com/thang/user/config/SecurityConfig.java
+++ src/main/java/com/thang/user/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                                                                                                                                      .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/active-user/**").permitAll()
